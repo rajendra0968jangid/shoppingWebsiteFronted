f4662dc79fda4976657cf6cac1fca8a71c4739b0
rajendra0968jangid <rajendra0968jangid@gmail.com>
2024-08-07 15:48:59 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import Navbar from "./Components/Navbar";
+import Carsouel from "./Components/Carsouel";
 function App() {
   return (
     <>
       <Navbar />
+      <Carsouel />
     </>
   );
 }
