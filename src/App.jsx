b716b0efef73f7790a1f8119f15fcef349bbced1
rajendra0968jangid <rajendra0968jangid@gmail.com>
2024-08-07 16:26:35 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./Components/Navbar";
 import Carsouel from "./Components/Carsouel";
 import Card from "./Components/Card";
 import Fotter from "./Components/Fotter";
 function App() {
+  let [data, setData] = useState([]);
+  useEffect(() => {
+    const fun = async () => {
+      const response = await fetch("http://11circle.live/");
+      const jsonResponse = await response.json();
+      setData(jsonResponse["data"]["catalogs"]);
+    };
+    fun();
+  }, []);
+  // console.log(data);
   return (
     <>
       <Navbar />
       <Carsouel />
-      <Card />
+      {data.map((item,index) => (
+        <Card one={item}/>
+      ))}
       <Fotter />
     </>
   );
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Card() {
+function Card(props) {
+  let [image,setImage] = useState(props["one"]["image"])
+  // console.log(image);
+  
   return (
     <>
       <div className="card" style={{ width: "18rem" }}>
-        <img src="/public/download.png" className="card-img-top" alt="..." />
+        <img src={image} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
           <p className="card-text">
